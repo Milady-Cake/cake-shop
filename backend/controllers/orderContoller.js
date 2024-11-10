21c4833fd5ed00f44a26512d8815b4133f21b22a
DEVA R <devarameshk2003@gmail.com>
2024-11-10 23:28:26 +0530

    Update orderContoller.js

--- backend/controllers/orderContoller.js
+++ backend/controllers/orderContoller.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 // placing user order for frontend
 const placeOrder = async (req, res) => {
-    const frontend_url = "https://cake-shop-frontend.onrender.com"
+    const frontend_url = 'https://cake-shop-frontend.onrender.com'
 
     try {
         const newOrder = new orderModel({
